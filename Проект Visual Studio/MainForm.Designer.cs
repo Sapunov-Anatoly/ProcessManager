9ab7556b35cfa49ce23856adda32f6a22a5438ac
Sapunov-Anatoly <101805645+Sapunov-Anatoly@users.noreply.github.com>
2022-12-28 03:25:00 +0700

    Add files via upload

---
+++ Проект Visual Studio/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ProcessManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.processDataGridView = new System.Windows.Forms.DataGridView();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.startProcessCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopProcessCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processIdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.processNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.processMemoryColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.processDataGridView)).BeginInit();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // processDataGridView
+            // 
+            this.processDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.processDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.processDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.processIdColumn,
+            this.processNameColumn,
+            this.processMemoryColumn});
+            this.processDataGridView.Cursor = System.Windows.Forms.Cursors.Default;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.processDataGridView.DefaultCellStyle = dataGridViewCellStyle8;
+            this.processDataGridView.Location = new System.Drawing.Point(12, 27);
+            this.processDataGridView.Name = "processDataGridView";
+            this.processDataGridView.RowHeadersVisible = false;
+            this.processDataGridView.Size = new System.Drawing.Size(760, 522);
+            this.processDataGridView.TabIndex = 0;
+            this.processDataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.processDataGridView_CellDoubleClick);
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.mainMenuStrip.AutoSize = false;
+            this.mainMenuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startProcessCheckToolStripMenuItem,
+            this.stopProcessCheckToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(9, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(300, 24);
+            this.mainMenuStrip.TabIndex = 1;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // startProcessCheckToolStripMenuItem
+            // 
+            this.startProcessCheckToolStripMenuItem.BackColor = System.Drawing.Color.LightGray;
+            this.startProcessCheckToolStripMenuItem.Enabled = false;
+            this.startProcessCheckToolStripMenuItem.Name = "startProcessCheckToolStripMenuItem";
+            this.startProcessCheckToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.startProcessCheckToolStripMenuItem.Text = "Пуск";
+            this.startProcessCheckToolStripMenuItem.Click += new System.EventHandler(this.startProcessCheckToolStripMenuItem_Click);
+            // 
+            // stopProcessCheckToolStripMenuItem
+            // 
+            this.stopProcessCheckToolStripMenuItem.BackColor = System.Drawing.Color.LightCoral;
+            this.stopProcessCheckToolStripMenuItem.Name = "stopProcessCheckToolStripMenuItem";
+            this.stopProcessCheckToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.stopProcessCheckToolStripMenuItem.Text = "Пауза";
+            this.stopProcessCheckToolStripMenuItem.Click += new System.EventHandler(this.stopProcessCheckToolStripMenuItem_Click);
+            // 
+            // processIdColumn
+            // 
+            this.processIdColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.processIdColumn.DefaultCellStyle = dataGridViewCellStyle5;
+            this.processIdColumn.HeaderText = "ID";
+            this.processIdColumn.Name = "processIdColumn";
+            this.processIdColumn.ReadOnly = true;
+            this.processIdColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // processNameColumn
+            // 
+            this.processNameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.processNameColumn.DefaultCellStyle = dataGridViewCellStyle6;
+            this.processNameColumn.FillWeight = 400F;
+            this.processNameColumn.HeaderText = "Имя процесса";
+            this.processNameColumn.Name = "processNameColumn";
+            this.processNameColumn.ReadOnly = true;
+            // 
+            // processMemoryColumn
+            // 
+            this.processMemoryColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.processMemoryColumn.DefaultCellStyle = dataGridViewCellStyle7;
+            this.processMemoryColumn.HeaderText = "Память (Мб)";
+            this.processMemoryColumn.Name = "processMemoryColumn";
+            this.processMemoryColumn.ReadOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.processDataGridView);
+            this.Controls.Add(this.mainMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProcessManager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.processDataGridView)).EndInit();
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView processDataGridView;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem startProcessCheckToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopProcessCheckToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn processIdColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn processNameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn processMemoryColumn;
+    }
+}
+
---
+++ Проект Visual Studio/MainForm.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+using Newtonsoft.Json;
+using Serilog;
+using Serilog.Core;
+using System.ComponentModel;
+
+namespace ProcessManager
+{
+    public partial class MainForm : Form
+    {
+        private class ConfigData
+        {
+            public int refreshRate { get; set; }
+        }
+
+        private Logger logFile;
+        private int refreshRate;
+        private static ManualResetEvent threadStatus = new ManualResetEvent( true );
+        private Thread getProcessesThread;
+        private IDictionary previousProcesses = new Dictionary<int, string>() { };
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            CreateLogFile();
+            InstallingRefreshRate();
+
+            StartThread();
+        }
+
+        private void CreateLogFile()
+        {
+            try
+            {
+                string pathToExe = Environment.CurrentDirectory.ToString();
+                string pathToLogFile = pathToExe + "\\Logs";
+                string logFileName = pathToLogFile + "\\Log_" + GetDate() + ".txt";
+
+                logFile = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File(logFileName).CreateLogger();
+            }
+            catch
+            {
+                MessageBox.Show("При создании журнала действий возникла непредвиденная ошибка, действия не будут записываться в отдельный файл.",
+                                "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void InstallingRefreshRate()
+        {
+            try
+            {
+                ConfigData returnedConfig = GetConfig();
+                refreshRate = returnedConfig.refreshRate * 1000;
+                logFile.Information("Частота обновления списка процессов установлена на " + returnedConfig.refreshRate + " сек.");
+            }
+            catch
+            {
+                MessageBox.Show("Файл настроек был потерян, либо содержит недопустимые символы. В качестве частоты обновления списка процессов будет установлена стандартная частота - 5 сек.",
+                                "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                refreshRate = 5000;
+
+                logFile.Error("Файл настроек был потерян, либо содержит недопустимые символы.");
+                logFile.Information("Частота обновления списка процессов установлена на " + refreshRate + " сек.");
+            }
+        }
+
+        static ConfigData GetConfig()
+        {
+            string getPathToExe = Environment.CurrentDirectory.ToString();
+            string pathToConfig = getPathToExe + "\\config.json";
+            string configText = File.ReadAllText(pathToConfig);
+            ConfigData returnedConfig = JsonConvert.DeserializeObject<ConfigData>(configText);
+
+            return returnedConfig;
+        }
+
+        private void processDataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int processId = Convert.ToInt32(processDataGridView.Rows[e.RowIndex].Cells[0].Value);
+
+                GetProcessInfo(processId);
+            }
+            catch { } // Для того случая, если пользователь будет кликать на названия столбцов
+        }
+
+        private void StartThread()
+        {
+            try
+            {
+                getProcessesThread = new Thread(UpdateProcessesList);
+                getProcessesThread.Start();
+
+                logFile.Information("Запущен мониторинг активных процессов.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                logFile.Warning("Программа завершена с ошибкой. " + ex.Message);
+                Application.Exit();
+            }
+        }
+
+        private void FindAddedAndDeletedProcesses(IDictionary actualProcesses)
+        {
+            try
+            {
+                if (previousProcesses.Count > 0)
+                {
+                    foreach (DictionaryEntry prevPoc in previousProcesses)
+                    {
+                        int procIdInCollection = Convert.ToInt32(prevPoc.Key);
+
+                        if (!actualProcesses.Contains(procIdInCollection))
+                        {
+                            actualProcesses.Remove(procIdInCollection);
+                            logFile.Debug("Удален процесс. ID: " + procIdInCollection + " Имя: " + prevPoc.Value);
+                        }
+                        else
+                        {
+                            actualProcesses.Remove(procIdInCollection);
+                        }
+                    }
+
+                    foreach (DictionaryEntry prevPoc in actualProcesses)
+                    {
+                        logFile.Debug("Добавлен процесс. ID: " + Convert.ToInt32(prevPoc.Key) + " Имя: " + prevPoc.Value);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                logFile.Warning("Не удалось получить информацию по удаленным и добавленным процессам. " + ex.Message);
+            }
+
+            actualProcesses.Clear();
+            previousProcesses.Clear();
+        }
+
+        private void UpdateProcessesList()
+        {
+            IDictionary actualProcesses = new Dictionary<int, string>() { };
+
+            while (true)
+            {
+                threadStatus.WaitOne();
+
+                Process[] processesList = Process.GetProcesses();
+
+                foreach (Process process in processesList)
+                {
+                    actualProcesses.Add(process.Id, process.ProcessName);
+                }
+
+                FindAddedAndDeletedProcesses(actualProcesses);
+
+                processDataGridView.Invoke((MethodInvoker)delegate
+                {
+                    processDataGridView.Rows.Clear();
+
+                    foreach (Process process in processesList)
+                    {
+                        processDataGridView.Rows.Add(process.Id,
+                                                     process.ProcessName,
+                                                     Math.Round(process.PagedMemorySize64 / 1048576.0, 2));
+
+                        previousProcesses.Add(process.Id, process.ProcessName);
+                    }
+
+                    this.processDataGridView.Sort(this.processDataGridView.Columns["processMemoryColumn"], ListSortDirection.Descending);
+                });
+
+                logFile.Information("Список активных процессов обновлен.");
+                Thread.Sleep(refreshRate);
+            } 
+        }
+
+        private void GetProcessInfo(int procId)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(procId);
+
+                MessageBox.Show("Имя процесса: " + process.ProcessName + "\n\n" +
+                                "Начало работы процесса: " + process.StartTime + "\n\n" +
+                                "Исполняемый файл: " + process.MainModule.ModuleName + "\n\n" +
+                                "Путь к исполняемому файлу: " + process.MainModule.FileName + "\n\n",
+                                "Свойства процесса", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                logFile.Information("Получена информация по процессу. ID: " + process.Id + " Имя: " + process.ProcessName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                logFile.Warning("Получение информации по процессу было завершено c ошибкой. " + ex.Message + ". ID процесса: " + procId);
+            }
+        }
+
+        static string GetDate()
+        {
+            string dateTime = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss").Replace(":", "_");
+            return dateTime;
+        }
+
+        private void startProcessCheckToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            threadStatus.Set();
+
+            startProcessCheckToolStripMenuItem.Enabled = false;
+            stopProcessCheckToolStripMenuItem.Enabled = true;
+            startProcessCheckToolStripMenuItem.BackColor = Color.LightGray;
+            stopProcessCheckToolStripMenuItem.BackColor = Color.LightCoral;
+
+            logFile.Information("Мониторинг активных процессов возобновлен.");
+        }
+
+        private void stopProcessCheckToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            threadStatus.Reset();
+
+            stopProcessCheckToolStripMenuItem.Enabled = false;
+            startProcessCheckToolStripMenuItem.Enabled = true;
+            stopProcessCheckToolStripMenuItem.BackColor = Color.LightGray;
+            startProcessCheckToolStripMenuItem.BackColor = Color.GreenYellow;
+
+            logFile.Information("Мониторинг активных процессов приостановлен.");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (getProcessesThread.IsAlive)
+                {
+                    getProcessesThread.Abort();
+                }
+            }
+            catch(Exception ex)
+            {
+                logFile.Warning("Завершение потока мониторинга завершено с ошибкой. " + ex.Message);
+            }
+
+            logFile.Information("Программа завершила свою работу.");
+        }    
+    }
+}
